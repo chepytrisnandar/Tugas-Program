e32daf89fccd537dd1f9f6fa2be8b3081dc0b944
chepytrisnandar <chepytrisnandar@gmail.com>
2017-04-11 04:14:00 +0700

    Add files via upload

---
+++ Pengulangan_10.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+main(){
+	int angka=10;
+	string huruf;
+	cout<<"Masukan Kalimat/Huruf : "<<endl;
+	cin>>huruf;
+	
+	int x;
+	for(x=0;x<10;x++){
+		cout<<huruf<<endl;
+	}
+	cout<<"Banyak Karakter dalam Array adalah :"<<x;
+}
